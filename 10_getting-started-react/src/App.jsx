8f3995fd1fad7bc99544bc4466fd4d5f4c746d08
barondedios <realbarondedios@gmail.com>
2025-08-27 13:19:47 +0800

    Activity 10: Add React About Me project

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Assignment 10: Getting Started with React – About Me</h1>
+
+      <h2>About Me:</h2>
+
+      <h6><em>Name:</em> Aaron Job B. Bacani</h6>
+      <h6><em>Course/Year:</em> BSIS 3-A</h6>
+      <h6><em>Fun Fact:</em> I can make character sketches really fast. I am also trying to learn Hangul (Korean) and Nihongo (Japanese)</h6>
+
+      <h2>Why I want to learn React?</h2>
+
+      <h6>
+       I'm excited to learn React because, not only this is our AppDev's topic this semester, but also because I want to build interactive web applications, and I’ve heard it’s one of the most in-demand skills in modern web development. I also enjoy working on front-end projects and making them dynamic and user-friendly and I think this will build my fundamentals and foundation needed to kickstart my career in IS.
+      </h6>
+
+    </div>
+  );
+}
+
+export default App;
