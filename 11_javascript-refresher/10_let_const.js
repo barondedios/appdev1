ce467324032f397678ae80319192b6d8a3a8a53b
barondedios <realbarondedios@gmail.com>
2025-09-06 01:03:09 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,14 @@
+var name = "Elmer"; 
+var age = 30;
+age = 20; 
+console.log("Using var:", name, age);
+
+let first_name = "Elmer"; 
+let my_age = 30;
+my_age = 20; 
+console.log("Using let:", first_name, my_age);
+
+// let your_name = "Aaron"
+// const your_age = 13;
+// your_age = 20;
+// console.log(your_age, name) //Nag e-error? kasi di pwede reassign?
