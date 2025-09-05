876c54088615584d8c5751440c4c318fbf766612
barondedios <realbarondedios@gmail.com>
2025-09-06 01:04:39 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,12 @@
+const greet = name => "Hello, " + name;
+console.log(greet("Aaron")); 
+
+const square = n => {
+  return n * n;
+};
+console.log(square(5)); 
+
+const sayHi = () => {
+  console.log("Hi!");
+};
+sayHi(); 
