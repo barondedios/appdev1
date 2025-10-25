5df7730619f71f340e201d6dce95a5a7a036eea8
barondedios <realbarondedios@gmail.com>
2025-10-25 09:38:38 +0800

    Done with when and how to create multi-component files

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -2,23 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-function Profile() {
-  return (
-    <img
-      src="https://i.imgur.com/GqTkybQ_d.webp?maxwidth=520&shape=thumb&fidelity=high"
-      alt="Photo of a historian"
-    />
-  );
-}
+import Profile from './Profile.jsx';
 
 export default function Gallery() {
   return (
     <section>
-      <h1>Amazing historians</h1>
+      <h1>Amazing scientists</h1>
       <Profile />
       <Profile />
       <Profile />
-    </section>
+    </section>  
   );
 }
+
