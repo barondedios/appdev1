fdb7f8a40b976673db31394130bd58cf13fb42b9
barondedios <realbarondedios@gmail.com>
2025-10-25 09:43:44 +0800

    Done with how to use curly braces with JSX to access JavaScript

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -4,22 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Profile from './Profile.jsx';
 
+const person = {
+  name: 'Zebra',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
 export default function TodoList() {
   return (
-    <>
-      <h1>Hedy Lamarr's Todos</h1>
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
       <img
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Hedy Lamarr"
-        className="photo"
+        className="avatar"
+        src="https://i.pinimg.com/736x/80/dc/cf/80dccf1b9be4d877a362b913b93b2258.jpg"
+        alt="Gregorio Y. Zara"
       />
       <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve spectrum technology</li>
+        <li>Eat</li>
+        <li>Sleep</li>
+        <li>Work, repeat</li>
       </ul>
-    </>
+    </div>
   );
 }
+
 
 
