280ee67641cfba8eeb0cada6aa6c91a40f6f886d
Baron <realbarondedios@gmail.com>
2025-11-09 18:05:17 +0800

    Done with exporting and importing a component

--- react-importing-exporting-components/src/App.jsx
+++ react-importing-exporting-components/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Greeting from './Greeting';
 
 function App() {
   return (
     <div>
       <h1>Welcome to Importing and Exporting Components</h1>
+      <Greeting />
     </div>
   );
 }
 
 export default App;
-
---
+++ react-importing-exporting-components/src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+  return <h2>Hello from the Greeting component!</h2>;
+}
+
+export default Greeting;
