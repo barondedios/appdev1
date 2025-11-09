0e5acfcde5122c1254b1a4aee593f33f2111dfb7
Baron <realbarondedios@gmail.com>
2025-11-09 18:07:24 +0800

    Done with exporting and importing multiple components from the same file

--- react-importing-exporting-components/src/App.jsx
+++ react-importing-exporting-components/src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Greeting from './Greeting';
+import Greeting from './Greeting';
+import { WelcomeMessage, GoodbyeMessage } from './Messages';
 
 function App() {
   return (
     <div>
       <h1>Welcome to Importing and Exporting Components</h1>
       <Greeting />
+      <WelcomeMessage />
+      <GoodbyeMessage />
     </div>
   );
 }
---
+++ react-importing-exporting-components/src/Messages.jsx
@@ -0,0 +1,7 @@
+export function WelcomeMessage() {
+  return <p>Welcome to our React learning journey!</p>;
+}
+
+export function GoodbyeMessage() {
+  return <p>Goodbye! See you in the next lesson.</p>;
+}
