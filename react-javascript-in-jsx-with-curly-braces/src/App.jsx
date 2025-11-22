4211c268028cbe69f584f08c9f81eee50a5d28d0
barondedios <realbarondedios@gmail.com>
2025-11-22 14:45:53 +0800

    Done with more fun with JavaScript objects and curly braces

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -3,17 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-export default function TodoList() {
+
+const dog = {
+  name: 'Browny',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
+export default function EatList() {
   return (
-    <ul style={
-      {
-      backgroundColor: 'black',
-      color: 'yellow'
-      }
-    }>
-      <li>Study react</li>
-      <li>Finish the deliverables in BPM</li>
-      <li>Finish laundry and clean the house</li>
-    </ul>
+    <div style={dog.theme}>
+      <h1>{dog.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="https://i.pinimg.com/736x/f1/97/40/f1974036c73e5421aced96730d6ed335.jpg"
+        alt="Browny"
+      />
+      <ul>
+        <li>Eat</li>
+        <li>Sleep</li>
+        <li>Repeat</li>
+      </ul>
+    </div>
   );
 }
