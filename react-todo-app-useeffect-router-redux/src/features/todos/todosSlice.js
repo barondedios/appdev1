dbe7360926dff360a3081bf74aea0bd8b9c138d7
Baron <realbarondedios@gmail.com>
2025-11-28 19:59:02 +0800

    Done with create todosSlice using Redux Toolkit section

---
+++ react-todo-app-useeffect-router-redux/src/features/todos/todosSlice.js
@@ -0,0 +1,30 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getTodos } from "../../../api/todosApi";
+
+export const fetchTodos = createAsyncThunk("todos/fetchTodos", async () => {
+  const res = await getTodos();
+  return res.data.slice(0, 10); // limit to 10
+});
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState: {
+    items: [],
+    status: "idle",
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTodos.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchTodos.fulfilled, (state, action) => {
+        state.status = "success";
+        state.items = action.payload;
+      })
+      .addCase(fetchTodos.rejected, (state) => {
+        state.status = "error";
+      });
+  },
+});
+
+export default todosSlice.reducer;
